4b7a64bb47a0df32cecfdfa8bde22a6cfdc9ad90
thetazero <lesha.seletskiy@gmail.com>
2023-05-19 10:07:19 -0700

    EKF core implementation

---
+++ src/__init__.py
@@ -0,0 +1,37 @@
+import numpy as np
+from autograd import jacobian
+
+
+class EKFCore:
+    def __init__(self, x0, P0, dynamics, measure, W, V) -> None:
+        self.x = x0
+        self.P = P0
+        self.f = dynamics
+        self.g = measure
+        self.W = W
+        self.V = V
+
+    def predict(self):
+        x_predicted = self.f(self.x)
+        A = jacobian(self.f)(x_predicted)
+        P_predicted = A @ self.P @ A.T + self.W
+        return x_predicted, P_predicted
+
+    def innovation(self, y, x_predicted, P_predicted):
+        y_predicted = self.g(x_predicted)
+        C = jacobian(self.g)(x_predicted)
+
+        Z = y - y_predicted
+        S = C @ P_predicted @ C.T + self.V
+        return Z, S, C
+
+    def kalman_gain(self, P_predicted, C, S):
+        return P_predicted @ C.T @ np.linalg.inv(S)
+
+    def update(self, y):
+        (x_predicted, P_predicted) = self.predict()
+        (Z, S, C) = self.innovation(y, x_predicted, P_predicted)
+        L = self.kalman_gain(P_predicted, C, S)
+        self.x = x_predicted + L @ Z
+        I = np.eye(self.P.shape[0])
+        self.P = (I - L @ C) @ P_predicted @ (I - L @ C).T + L @ self.V @ L.T
---
+++ tests/example.py
@@ -0,0 +1,26 @@
+from src import EKFCore
+import autograd.numpy as np
+import unittest
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+# State is defined at
+# [x, y, vx, vy]
+
+
+class TestBasicEKFConvergence(unittest.TestCase):
+
+    def test_2d_point(self):
+
+        def dynamics(x):
+            return np.array([x[0]+x[2], x[1]+x[3], x[2], x[3]])
+
+        def measure(x):
+            return np.array([x[0], x[1]])
+
+        x0 = np.array([0.0, 0.0, 0.0, 0.0])
+        P0 = np.eye(4)
+        EKF = EKFCore(x0, P0, dynamics, measure, np.eye(4), np.eye(2))
+
+        for i in range(100):
+            EKF.update(np.array([0.4+i*0.1, 0.4+i*0.3]))
+        assert_array_almost_equal(EKF.x, np.array([10.3, 30.3, 0.1, 0.3]))
